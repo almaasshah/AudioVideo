f5342d4e131bc08e518ecc1b84e6983fb584fdd2
S-Shah <almaasshah>
2018-11-30 11:32:07 +0000

    SeekBar Added.

--- app/src/main/java/com/example/s_shah/audiovideo/MainActivity.java
+++ app/src/main/java/com/example/s_shah/audiovideo/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.s_shah.audiovideo;
 
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
@@ -7,6 +8,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.MediaController;
+import android.widget.SeekBar;
 import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
@@ -17,6 +19,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Button playmusicbtn;
     private Button stopmusicbtn;
     private MediaPlayer mediaPlayer;
+    private SeekBar seekBar;
+    private AudioManager audioManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,14 +31,40 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         play_btn = findViewById(R.id.play_btn);
         playmusicbtn = findViewById(R.id.playmusicbtn);
         stopmusicbtn = findViewById(R.id.stopmusicbtn);
-
+        seekBar = findViewById(R.id.seekBar);
 
        play_btn.setOnClickListener(MainActivity.this);
        playmusicbtn.setOnClickListener(MainActivity.this);
        stopmusicbtn.setOnClickListener(MainActivity.this);
 
+
        mediaController = new MediaController(MainActivity.this);
        mediaPlayer = MediaPlayer.create(this,R.raw.heartbeat);
+       audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+
+       int maximumVolumeOfUserDevice = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+       int currentVolumeOfUserDevice = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+
+       seekBar.setMax(maximumVolumeOfUserDevice);
+       seekBar.setProgress(currentVolumeOfUserDevice);
+
+       seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+           @Override
+           public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+           }
+
+           @Override
+           public void onStartTrackingTouch(SeekBar seekBar) {
+
+           }
+
+           @Override
+           public void onStopTrackingTouch(SeekBar seekBar) {
+
+           }
+       });
+
 
     }
 
